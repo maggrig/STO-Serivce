ef9e3863fa61e57883fceafb05cb950ec9752ec5
Grigoriy <Grigoriy1@rambler.ru>
2023-04-01 23:32:58 +0300

    login form without "roles"

--- src/main/java/mag/grig/controller/MyErrorController.java
+++ src/main/java/mag/grig/controller/MyErrorController.java
@@ -6,7 +6,6 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MyErrorController implements ErrorController {
@@ -19,12 +18,12 @@ public class MyErrorController implements ErrorController {
             Integer statusCode = Integer.valueOf(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error-404";
+                return "error404";
             }  if(statusCode == 403) {
                 return "error403";
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
+                return "error500";
             }
         }
         return "error";
